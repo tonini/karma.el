1b2c9661ddb0d5a5a61901d485719c0b389dd258
Samuel Tonini <tonini.samuel@gmail.com>
2014-11-03 22:21:30 +0100

    refactoring whole package

--- karma.el
+++ karma.el
@@ -31,20 +31,21 @@
 
 
 ;;; Commentary:
-;;
-
 
 ;;; Code:
 
 (require 'compile)
+(require 'ansi-color)
 
 (defcustom karma-command "karma"
-  "The shell command karma."
+  "The shell command for karma."
   :type 'string
   :group 'karma)
 
-(defvar karma-config-file ".karma"
-  "The file which contains the path to the karma conf file.")
+(defcustom karma-config-file ".karma"
+  "The file which contains the path to the karma conf file."
+  :type 'string
+  :group 'karma)
 
 (defvar karma-start-buffer-name "*karma start*"
   "Name of the karma server output buffer.")
@@ -52,23 +53,25 @@
 (defvar karma-run-buffer-name "*karma run*"
   "Name of the karma run output buffer.")
 
-(defvar karma-buffer-name "*karma*"
+(defvar karma-server-buffer-name "*karma server*"
   "Name of the karma server output buffer.")
 
-(defvar karma--compilation-buffer-name nil
-  "Used to store compilation name so recompilation works as expected.")
-(make-variable-buffer-local 'karma--compilation-buffer-name)
+(defun karma--flatten (alist)
+  (cond ((null alist) nil)
+        ((atom alist) (list alist))
+        (t (append (karma--flatten (car alist))
+                   (karma--flatten (cdr alist))))))
+
+(defun karma--build-runner-cmdlist (command)
+  "Build the commands list for the runner."
+  (remove "" (karma--flatten
+              (list karma-command (if (stringp command)
+                                      (split-string command)
+                                    command)))))
 
 (defvar karma--project-root-indicators
   '("package.json" "bower.json")
-  "List of files which indicates a javascript project root.")
-
-(defun karma-flatten (alist)
-  "Flatten any lists within ALIST, so that there are no sublists."
-  (cond ((null alist) nil)
-        ((atom alist) (list alist))
-        (t (append (karma-flatten (car alist))
-                   (karma-flatten (cdr alist))))))
+  "list of file-/directory-names which indicate a root of a elixir project")
 
 (defun karma-project-root ()
   (let ((file (file-name-as-directory (expand-file-name default-directory))))
@@ -80,35 +83,14 @@
           (indicators (karma--project-root-identifier file (cdr indicators)))
           (t nil))))
 
-(defun karma-establish-root-directory ()
+(defun karma--establish-root-directory ()
   "Set the default-directory to the karma used project root."
   (let ((project-root (karma-project-root)))
     (if (not project-root)
         (error "Couldn't find any project root")
       (setq default-directory project-root))))
 
-(defun karma-build-compile-cmdlist (command)
-  "Build the command list for `karma-compile' from COMMAND."
-  (remove "" (karma-flatten
-              (list karma-command (if (stringp command)
-                                      (split-string command)
-                                    command)))))
-
-(define-compilation-mode karma-compilation-mode "Karma"
-  "Karma compilation mode.")
-
-(defun karma-compile (name cmdlist)
-  "In a buffer identified by NAME, run CMDLIST in `karma-compilation-mode'."
-  (save-some-buffers (not compilation-ask-about-save)
-                     (when (boundp 'compilation-save-buffers-predicate)
-                       compilation-save-buffers-predicate))
-  (let* ((karma--compilation-buffer-name name)
-         (compilation-scroll-output t))
-    (with-current-buffer
-        (compilation-start (format "%s" cmdlist) 'karma-compilation-mode
-                           (lambda (b) karma--compilation-buffer-name)))))
-
-(defun karma-config-path-to-file ()
+(defun karma-path-to-config-file ()
   (let ((file (format "%s/%s" (karma-project-root) karma-config-file)))
     (if (not (file-exists-p file))
         (error "Couldn't find any karma config file."))
@@ -116,40 +98,100 @@
       (insert-file-contents file)
       (expand-file-name (buffer-string) (karma-project-root)))))
 
+(defvar karma-buffer--buffer-name nil
+  "Used to store compilation name so recompilation works as expected.")
+(make-variable-buffer-local 'karma-buffer--buffer-name)
+
+(defvar karma-buffer--error-link-options
+  '(karma "\\([-A-Za-z0-9./_]+\\):\\([0-9]+\\)\\(: warning\\)?" 1 2 nil (3) 1)
+  "File link matcher for `compilation-error-regexp-alist-alist' (matches path/to/file:line).")
+
+(defun karma-buffer--kill-any-orphan-proc ()
+  "Ensure any dangling buffer process is killed."
+  (let ((orphan-proc (get-buffer-process (buffer-name))))
+    (when orphan-proc
+      (kill-process orphan-proc))))
+
+(define-compilation-mode karma-buffer-mode "Karma"
+  "Karma compilation mode."
+  (progn
+    (font-lock-add-keywords nil
+                            '(("^Finished in .*$" . font-lock-string-face)
+                              ("^Karma.*$" . font-lock-string-face)))
+    ;; Set any bound buffer name buffer-locally
+    (setq karma-buffer--buffer-name karma-buffer--buffer-name)
+    (set (make-local-variable 'kill-buffer-hook)
+         'karma-buffer--kill-any-orphan-proc)))
+
+(defvar karma-buffer--save-buffers-predicate
+  (lambda ()
+    (not (string= (substring (buffer-name) 0 1) "*"))))
+
+(defun karma-buffer--handle-compilation-once ()
+  (remove-hook 'compilation-filter-hook 'karma-buffer--handle-compilation-once t)
+  (delete-matching-lines "\\(-*- mode:\\|^$\\|karma run\\|Loading config\\|--no-single-run\\|Karma finished\\|Karma started\\|karma-compilation;\\)"
+                         (point-min) (point)))
+
+(defun karma-buffer--handle-compilation ()
+  (ansi-color-apply-on-region (point-min) (point-max)))
+
+(defun karma-compilation-run (cmdlist buffer-name)
+  "Run CMDLIST in `buffer-name'.
+Returns the compilation buffer.
+Argument BUFFER-NAME for the compilation."
+  (save-some-buffers (not compilation-ask-about-save) karma-buffer--save-buffers-predicate)
+  (let* ((karma-buffer--buffer-name buffer-name)
+         (compilation-filter-start (point-min)))
+    (with-current-buffer
+        (compilation-start (mapconcat 'shell-quote-argument cmdlist " ")
+                           'karma-buffer-mode
+                           (lambda (b) karma-buffer--buffer-name))
+      (setq-local compilation-error-regexp-alist-alist
+                  (cons karma-buffer--error-link-options compilation-error-regexp-alist-alist))
+      (setq-local compilation-error-regexp-alist (cons 'karma compilation-error-regexp-alist))
+      (add-hook 'compilation-filter-hook 'karma-buffer--handle-compilation nil t)
+      (add-hook 'compilation-filter-hook 'karma-buffer--handle-compilation-once nil t))))
+
 (defun karma-start ()
   (interactive)
-  (karma-execute (list "start" (karma-config-path-to-file))
+  (karma-execute (list "start" (karma-path-to-config-file))
                  karma-start-buffer-name))
-
-(defun karma-start-with-prompt (args)
-  (interactive "Mkarma start: ")
-  (karma-execute (list "start" args) karma-start-buffer-name))
 
 (defun karma-start-single-run ()
   (interactive)
-  (karma-execute (list "start" (karma-config-path-to-file) "--single-run")
+  (karma-execute (list "start" (karma-path-to-config-file) "--single-run")
                  karma-start-buffer-name))
 
 (defun karma-start-no-single-run ()
   (interactive)
-  (karma-execute (list "start" (karma-config-path-to-file) "--no-single-run")
+  (karma-execute (list "start" (karma-path-to-config-file) "--no-single-run")
                  karma-start-buffer-name))
 
 (defun karma-run ()
   (interactive)
-  (karma-execute (list "run") karma-run-buffer-name))
+  (karma-execute (list "run" (karma-path-to-config-file))
+                 karma-run-buffer-name))
 
-(defun karma-run-with-prompt (args)
-  (interactive "Mkarma run: ")
-  (karma-execute (list "run" args) karma-run-buffer-name))
-
-(defun karma-execute (cmdlist &optional buffer-name)
-  "Run a karma command with CMDLIST as arguments."
+(defun karma-execute (cmdlist buffer-name)
+  "Run a karma command."
   (interactive "Mkarma: ")
-  (let ((name (if buffer-name buffer-name
-                karma-buffer-name)))
-    (karma-establish-root-directory)
-    (karma-compile name (karma-build-compile-cmdlist cmdlist))))
+  (let ((old-directory default-directory))
+    (karma--establish-root-directory)
+    (karma-compilation-run (karma--build-runner-cmdlist cmdlist)
+                           buffer-name)
+    (cd old-directory)))
+
+;;;###autoload
+(define-minor-mode karma-mode
+  "Toggle karma mode.
+
+Key bindings:
+\\{karma-mode-map}"
+  nil
+  ;; The indicator for the mode line.
+  " karma"
+  :group 'karma
+  :global t)
 
 (provide 'karma)
 
--- test/karma-tests.el
+++ test/karma-tests.el
@@ -52,29 +52,29 @@
    (should (equal (karma-project-root) nil))))
 
 (ert-deftest test-flatten-of-list ()
-  (should (equal (karma-flatten '(1 2 (3 4) 5))
+  (should (equal (karma--flatten '(1 2 (3 4) 5))
                  '(1 2 3 4 5)))
-  (should (equal (karma-flatten '(1 2 ("dude" "hero" (3)) 4 5))
+  (should (equal (karma--flatten '(1 2 ("dude" "hero" (3)) 4 5))
                  '(1 2 "dude" "hero" 3 4 5))))
 
 (ert-deftest test-establish-root-directory/set-default-directory ()
   (within-sandbox "lib/bower"
                   (f-touch "../../bower.json")
-                  (should (equal (karma-establish-root-directory)
+                  (should (equal (karma--establish-root-directory)
                                  default-directory))))
 
 (ert-deftest test-establish-root-directory/no-root-exists ()
   (within-sandbox
-   (should-error (karma-establish-root-directory))))
+   (should-error (karma--establish-root-directory))))
 
 (ert-deftest test-build-compile-cmdlist ()
-  (should (equal (karma-build-compile-cmdlist "start")
+  (should (equal (karma--build-runner-cmdlist "start")
                  '("karma" "start")))
-  (should (equal (karma-build-compile-cmdlist '("run"))
+  (should (equal (karma--build-runner-cmdlist '("run"))
                  '("karma" "run")))
-  (should (equal (karma-build-compile-cmdlist "start --help")
+  (should (equal (karma--build-runner-cmdlist "start --help")
                  '("karma" "start" "--help")))
-  (should (equal (karma-build-compile-cmdlist '("run" ""))
+  (should (equal (karma--build-runner-cmdlist '("run" ""))
                  '("karma" "run"))))
 
 (provide 'karma-tests)
