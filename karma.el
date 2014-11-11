418d4da74e336fa947d7ebaf6795a4b4a1bdbb9a
Samuel Tonini <tonini.samuel@gmail.com>
2014-11-11 21:01:33 +0100

    update inline commentary

--- karma.el
+++ karma.el
@@ -6,31 +6,55 @@
 ;; Maintainer: Samuel Tonini
 ;; Version: 0.1.0
 ;; URL: http://github.com/tonini/karma.el
-;; Keywords: javascript, karma, testing
+;; Keywords: javascript, js, karma, testing
 
-;; The MIT License (MIT)
-;;
-;; Copyright (c) Samuel Tonini
-;;
-;; Permission is hereby granted, free of charge, to any person obtaining a copy of
-;; this software and associated documentation files (the "Software"), to deal in
-;; the Software without restriction, including without limitation the rights to
-;; use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-;; the Software, and to permit persons to whom the Software is furnished to do so,
-;; subject to the following conditions:
-;;
-;; The above copyright notice and this permission notice shall be included in all
-;; copies or substantial portions of the Software.
-;;
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-;; FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-;; COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-;; IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;; This file is not part of GNU Emacs.
 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
+;;  Installation:
+
+;;    karma.el is available on community maintained repository MELPA.
+;;
+;;    (add-to-list 'package-archives
+;;                 '("melpa" . "http://melpa.org/packages/") t)
+;;
+;;    M-x package-install [RET] karma [RET]
+
+;;  Manual Installation:
+;;
+;;    (add-to-list 'load-path "~/path/to/karma.el/")
+;;    (require 'karma)
+
+;;  Usage:
+
+;;  Basic setup:
+
+;;    You need to create an `.karma` file inside your project directory to inform
+;;    Karma.el where to get the Karma config file and the Karma executable.
+
+;;    Example .karma file:
+
+;;      {
+;;        "config-file": "karma.coffee",
+;;        "karma-command": "node_modules/karma/bin/karma"
+;;      }
+
+;;    The `config-file` and the `karma-command` paths need to be relative or absoulte
+;;    to the your project directory.
 
 ;;; Code:
 
